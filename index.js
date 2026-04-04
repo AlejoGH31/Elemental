60ac3f2612db53a6d5526c3999c3a2ff9d3d238b
Alejandro <alejandroochoamartinez2024@gmail.com>
2026-04-04 18:58:10 -0500

    test2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Servidor y dependencias/librerias
 const express = require("express")
 const cors = require("cors")
 
@@ -7,8 +8,10 @@ app.use(express.static("public"))
 app.use(cors())
 app.use(express.json())
 
+//variable globla que almacena a todos los jugadores
 let jugadores = []
 
+// clase con toda la informacion de cada jugador
 class Jugador {
     constructor(id) {
         this.id = id
@@ -29,12 +32,14 @@ class Jugador {
     }
 }
 
+// la mascota de cada jugador
 class Mascota {
     constructor(nombre) {
         this.nombre = nombre
     }
 }
 
+// asigna un id aleatorio entre 0 y 1 para cada jugador
 app.get("/unirse", (req, res) => {
     const id = `${Math.random()}`
 
@@ -47,10 +52,10 @@ app.get("/unirse", (req, res) => {
     res.send(id)
 })
 
-
+// manda toda la informacion del jugador al frontend
 app.post("/elemental/:jugadorId", (req, res) => {
     const jugadorId = req.params.jugadorId || ""
-    const nombre = req.body.elemental || ""
+    const nombre = req.body.mascota || ""
     const mascota = new Mascota(nombre)
 
     const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
@@ -64,6 +69,7 @@ app.post("/elemental/:jugadorId", (req, res) => {
     res.end()
 })
 
+// manda la posicion de los jugadores existentes al frontend
 app.post("/elemental/:jugadorId/posicion", (req, res) => {
     const { x, y } = req.body
     const { jugadorId } = req.params
@@ -82,6 +88,7 @@ app.post("/elemental/:jugadorId/posicion", (req, res) => {
     res.json({ enemigos })
 })
 
+// manda el sistema de desconexion por inactividad al frontend
 app.post("/disconnect", (req, res) => {
     const { jugadorId } = req.body
 
@@ -92,6 +99,7 @@ app.post("/disconnect", (req, res) => {
     res.sendStatus(200)
 })
 
+// manda el sistema de desconexion por heartbeat al frontend
 app.post("/heartbeat", (req, res) => {
     const { jugadorId } = req.body
 
@@ -104,6 +112,7 @@ app.post("/heartbeat", (req, res) => {
     res.sendStatus(200)
 })
 
+// manda el conjunto de ataques de cada jugador al frontend
 app.post("/elemental/:jugadorId/ataques", (req, res) => {
     const jugadorId = req.params.jugadorId || ""
     const ataques = req.body.ataques || []
@@ -117,6 +126,7 @@ app.post("/elemental/:jugadorId/ataques", (req, res) => {
     res.end()
 })
 
+// obtiene los ataques del enemigo y manda los ataques propios al frontend
 app.get("/elemental/:jugadorId/ataques", (req, res) => {
     const jugadorId = req.params.jugadorId || ""
     const jugador = jugadores.find((jugador) => jugador.id === jugadorId)
@@ -125,8 +135,10 @@ app.get("/elemental/:jugadorId/ataques", (req, res) => {
     })
 })
 
+// puerto del servidor en local
 const PORT = process.env.PORT || 8080
 
+// cada 2 segundos se evalua si el jugador sigue activo, de lo contrario se desconecta
 setInterval(() => {
 
     const ahora = Date.now()
--- public/scriptElemental.js
+++ public/scriptElemental.js
@@ -1,3 +1,4 @@
+// referencias del html
 const sectionAtaque = document.getElementById("SeleccionarAtaque")
 const sectionReiniciar = document.getElementById("ReiniciarJuego")
 const botonMascota = document.getElementById("boton-seleccionar")
@@ -32,16 +33,18 @@ const btnCreditos = document.getElementById("boton-creditos")
 const pantallaCreditos = document.getElementById("pantalla-creditos")
 const regresarCreditos = document.getElementById("regresar-creditos")
 
-const ENV = "dev" // cambia a "prod" cuando publiques
+const ENV = "dev" // separa entornos dev y prod para hacer pruebas locales y despliegue en render.com
 
+//detecta automaciamente el entorno y asigna la URL del backend según corresponda
 const API_URL = window.location.hostname === "localhost"
     ? "http://192.168.1.17:8080"
     : "https://elemental-754y.onrender.com"
 
-let jugadorId = null
-let enemigoId = null
-let personajes = []
-let personajesEnemigos = []
+// variables globales
+let jugadorId = null // id de cada jugador
+let enemigoId = null // id de cada enemigo
+let personajes = [] // personajes disponibles para jugar
+let personajesEnemigos = [] // todos los personajes enemigos que se van a mostrar en el mapa
 let ataqueJugador = []
 let ataqueEnemigo = []
 let opcionDePersonajes
@@ -49,39 +52,40 @@ let mascota1
 let mascota2
 let mascota3
 let guardarMascota
-let miPersonaje
-let ataquesPersonajeEnemigo = []
-let ataqueDinamicoJugador = []
+let miPersonaje //personaje del jugador
+let ataquesPersonajeEnemigo = [] // botones de ataque dinamicos, segun el personaje elegido por el enemigo
+let ataqueDinamicoJugador = [] // botones de ataque dinamicos, segun el personaje elegido por el jugador
 let botonFuego
 let botonAgua
 let botonTierra
-let botones = []
+let botones = [] 
 let indexAtaqueJugador
 let indexAtaqueEnemigo
 let victoriasJugador = 0
 let victoriasEnemigo = 0
 let vidasJugador = 3
 let vidasEnemigo = 3
-let lienzo = mapaJuego.getContext("2d")
-let intervalo
+let lienzo = mapaJuego.getContext("2d") // el mapa
+let intervalo // indica cada cuanto tiempo se realizan ciertas acciones, como pintar el mapa, obtener ataques del enemigo, etc
 let fondoBatalla = new Image()
 fondoBatalla.src = "./assets/fondo-batalla.png" 
 let buscarAltura
 let anchoDelMapa = window.innerWidth - 20
 const anchoMaximoMapa = 500
 let opcionDeAtaques
-let enMapa = false
-let ultimoMovimiento = Date.now()
+let enMapa = false // controla si el jugador esta en el mapa para el sistema de desconexión por inactividad
+let ultimoMovimiento = Date.now() //variables globales
 
 if (anchoDelMapa > anchoMaximoMapa) {
     anchoDelMapa = anchoMaximoMapa - 20
-}
+}                                      
 
 buscarAltura = anchoDelMapa * 600 / 800
 
 mapaJuego.width = anchoDelMapa
 mapaJuego.height = buscarAltura
 
+//el constructor de cada personaje, con sus atributos y métodos para pintar su imagen en el mapa
 class Personaje {
     constructor(nombre, foto, vidas, fotoCara, id = null) {
         this.id = id
@@ -102,6 +106,7 @@ class Personaje {
         this.velocidadY = 0
     }
 
+//funcion de clase, pinta el personaje y sus coordenadas
     pintarMascota() {
         lienzo.drawImage(
             this.fotoCara,
@@ -113,12 +118,14 @@ class Personaje {
     }
 }
 
-let aquanut = new Personaje("Aquanut", "./assets/aquanut-juego.png", 3, "./assets/aquanut-cara.png")
+//crea los personajes jugables
+let aquanut = new Personaje("Aquanut", "./assets/aquanut-juego.png", 3, "./assets/aquanut-cara.png") 
 
 let drakon = new Personaje("Drakon", "./assets/drakon-personaje.png", 3, "./assets/drakon-cara.png")
 
 let selvatron = new Personaje("Selvatron", "./assets/Selvatron-personaje.png", 3, "./assets/selvatron-cara.png")
 
+// conjunto de ataques de cada personaje
 const aquanut_ataques = [
     {nombre: "💧", id: "boton-agua"},
     {nombre: "💧", id: "boton-agua"},
@@ -143,19 +150,22 @@ const selvatron_ataques = [
     {nombre: "💧", id: "boton-agua"},
 ]
 
+// asigna los ataques a cada personaje
 aquanut.ataques.push(...aquanut_ataques)
 
 drakon.ataques.push(...drakon_ataques)
 
 selvatron.ataques.push(...selvatron_ataques)
 
+// lista qur guarda todos los personajes
 personajes.push(aquanut, drakon, selvatron)
 
+//funcion que cancela todas las funciones y el pintado de un personaje que ya no esta en el juego
 function desconectarJugador() {
     console.log("Jugador desconectado completamente")
     enMapa = false
     
-    // Enviar al servidor primero
+    // recibe la orden del servidor para ejecutar la funcion
     fetch(`${API_URL}/disconnect`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -172,15 +182,10 @@ function desconectarJugador() {
     alert("Desconectado por inactividad")
 }
 
+// inicia toda la logica basica del juego
 function iniciarJuego() {
     enMapa = false
     unirseJuego()
-    sectionMenuPrincipal.style.display = "flex"
-    sectionMascota.style.display = "none"
-    sectionAtaque.style.display = "none"
-    sectionReiniciar.style.display = "none"
-    sectionCanvasJuego.style.display = "none"
-    pantallaCreditos.style.display = "none"
 
     personajes.forEach((personaje) => {
         opcionDePersonajes = `
@@ -198,17 +203,22 @@ function iniciarJuego() {
         mascota3 = document.getElementById("Selvatron")
     })
 
+    sectionMenuPrincipal.style.display = "flex"
+    sectionMascota.style.display = "none"
+    sectionAtaque.style.display = "none"
+    sectionReiniciar.style.display = "none"
+    pantallaCreditos.style.display = "none"
+    sectionCanvasJuego.style.display = "none"
+
     botonJugar.addEventListener("click", empezarJuego)
     quitarJuego.addEventListener("click", cerrarJuego)
     btnCreditos.addEventListener("click", mostrarCreditos)
-
     botonMascota.addEventListener("click", seleccionarMascota)
-
     botonReiniciar.addEventListener("click", reiniciarJuego)
 }
 
+// cada segundo ejecuta una funcion que evalua si el jugador esta inactivo, despues de 5 segundos de inactividad se desconecta al jugador
 setInterval(() => {
-
     if (!enMapa) return // 👈 SOLO en mapa
 
     const ahora = Date.now()
@@ -225,9 +235,9 @@ setInterval(() => {
 
         desconectarJugador() 
     }
-
 }, 1000)
 
+// cada 2 segundos le manda actividad al servidor, si lleva 2 segundos sin actividad DENTRO del mapa, tambien desconecta al jugador ya que beforeunload puede fallar
 setInterval(() => {
 
     if (!jugadorId) return
@@ -244,31 +254,36 @@ setInterval(() => {
 
 }, 2000)
 
+// evalua si el jugador esta en dispositivo movil, si es asi muestra las flechas de movimiento, de lo contrario las oculta porque en pc se usan las teclas fisicas para moverse
 function dispositivoMovil() {
     const pantallaTouch = navigator.maxTouchPoints > 0 || "ontouchstart" in window;
+
     return pantallaTouch
 }
 
-    if (dispositivoMovil()) {
+if (dispositivoMovil()) {
         flechasDispositivo.style.display = "flex"
     } else {
         flechasDispositivo.style.display = "none"
     }
 
+// funcion para mostras los creditos
 function mostrarCreditos() {
     sectionMenuPrincipal.style.display = "none"
     pantallaCreditos.style.display = "flex"
     enMapa = false 
 }
 
-regresarCreditos.addEventListener("click", quitarCreditos)
-
+// funcion para quitar los creditos
 function quitarCreditos() {
     pantallaCreditos.style.display = "none"
     sectionMenuPrincipal.style.display = "flex"
     enMapa = false 
 }
 
+regresarCreditos.addEventListener("click", quitarCreditos)
+
+// asigna un id aleatorio entre 0 y 1 a cada persona que entra al juego
 function unirseJuego() {
     fetch(`${API_URL}/unirse`)
         .then(function (res) {
@@ -283,6 +298,7 @@ function unirseJuego() {
         })
 }
 
+// detecta si el jugador cerró la pestaña o la ventana con el juego (inseguro) tiene respaldo de heartbeat (seguro)
 window.addEventListener("beforeunload", () => {
     const data = new Blob(
         [JSON.stringify({ jugadorId: jugadorId })],
@@ -292,6 +308,7 @@ window.addEventListener("beforeunload", () => {
     navigator.sendBeacon(`${API_URL}/disconnect`, data)
 })
 
+// oculta la seccion de elegir mascota y muestra el mapa del juego
 function empezarJuego() {
     sectionMenuPrincipal.style.display = "none"
     sectionMascota.style.display = "flex"
@@ -299,10 +316,12 @@ function empezarJuego() {
     enMapa = false
 }
 
+// cierra el juego completo
 function cerrarJuego() {
     window.close();
 }
 
+// funcion para elegir mascota y guardar el id de la mascota
 function seleccionarMascota() {
     if(mascota1.checked) {
         spanMascotaJugador.innerHTML = mascota1.id
@@ -321,11 +340,11 @@ function seleccionarMascota() {
     sectionCanvasJuego.style.display = "flex"
 
     mascotaSeleccionada(guardarMascota)
-
     extraerAtaques(guardarMascota)
     iniciarMapa()
 }
 
+// le comunica al servidor que mascota eligio el jugador para mostrarlo a los demas jugadores
 function mascotaSeleccionada(guardarMascota) {
     fetch(`${API_URL}/elemental/${jugadorId}`, {
         method: "post",
@@ -333,12 +352,12 @@ function mascotaSeleccionada(guardarMascota) {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            elemental: guardarMascota
+            mascota: guardarMascota
         })
-    
     })
 }
 
+// inicia el mapa y la logica del jugador en el mapa
 function iniciarMapa() {
     miPersonaje = obtenerObjetoMascota(guardarMascota)
     intervalo = setInterval(pintarMascotaYJuego, 100)
@@ -359,6 +378,7 @@ function extraerAtaques(guardarMascota) {
     mostrarAtaques(ataquesDPP)
 }
 
+// muestra los ataques dinamicos
 function mostrarAtaques(ataquesDPP) {
     ataquesDPP.forEach((ataque) => {
         opcionDeAtaques = `
@@ -373,6 +393,7 @@ function mostrarAtaques(ataquesDPP) {
     botones = document.querySelectorAll(".BAtaque")
 }
 
+// evalua que ataques fueron seleccionas y desactiva el boton del ataque seleccionado y despues de cada ataque evalua si el jugador seleccionó 5 ataques para mandarlo al servidor
 function secuenciaAtaque() {
     botones.forEach((boton) => {
         boton.addEventListener("click", (e) => {
@@ -395,6 +416,7 @@ function secuenciaAtaque() {
                 boton.style.background = "orange"
                 boton.disabled = true
             }
+
             if (ataqueDinamicoJugador.length === 5) {
                 enviarAtaques()
             }
@@ -402,12 +424,14 @@ function secuenciaAtaque() {
     })
 }
 
+// la informarcion de la mascota enemiga
 function seleccionarMascotaEnemigo(enemigo) {
     spanMascotaEnemigo.innerHTML = enemigo.nombre
     ataquesPersonajeEnemigo = enemigo.ataques
     secuenciaAtaque()
 }
 
+// envia los ataques de ambos jugadores al servidor y cada 50ms obtiene los ataques del enemigo
 function enviarAtaques() {
     fetch (`${API_URL}/elemental/${jugadorId}/ataques`, {
         method: "post",
@@ -421,6 +445,7 @@ function enviarAtaques() {
     intervalo = setInterval(obtenerAtaques, 50)
 }
 
+//obtiene los ataques de ambos jugadores, y si ambos seleccionaron 5 ataque se muestra el resultdado del combate
 function obtenerAtaques() {
     fetch(`${API_URL}/elemental/${enemigoId}/ataques`)
         .then(function (res) {
@@ -436,41 +461,13 @@ function obtenerAtaques() {
         })
 }
 
-function ataqueAleatorioEnemigo() {
-    let ataqueAleatorio = aleatorio(0, ataquesPersonajeEnemigo.length - 1)
-
-    if (ataqueAleatorio === 0 || ataqueAleatorio === 1) {
-        ataqueEnemigo.push("FUEGO")
-        indexAtaqueEnemigo = "FUEGO🔥"
-        console.log(ataqueEnemigo)
-    } else if (ataqueAleatorio === 2 || ataqueAleatorio === 3) {
-        ataqueEnemigo.push("AGUA")
-        indexAtaqueEnemigo = "AGUA💧"
-        console.log(ataqueEnemigo)
-    } else {
-        ataqueEnemigo.push("TIERRA")
-        indexAtaqueEnemigo = "TIERRA🌱"
-        console.log(ataqueEnemigo)
-    }
-    iniciarPelea()
-}
-
-function iniciarPelea() {
-    if (
-    ataques.length === 5 &&
-    ataqueDinamicoJugador.length === 5 &&
-    !combateEjecutado
-    ) {
-    combateEjecutado = true
-    ataqueEnemigo = ataques
-    combate()
-    }
-}
+// asigna el ataque seleccionado por cada jugador para mostrarlo en el resultado del combate
 function indexAmbosJugadores(jugador, enemigo) {
     indexAtaqueJugador = ataqueDinamicoJugador[jugador]
     indexAtaqueEnemigo = ataqueEnemigo[enemigo]
 }
 
+//funcion de combate, se evalua los ataques de ambos jugadores y se determina que jugador gana y cual pierde, ejm(J1: Fuego, J2: Agua. J2 pierde) y se crea un mensaje para mostrar en el cuadro de informacion los ataques que lanzaron ambos jugadores y en el orden en que lo hicieron, el jugador que gana se le suman victorias dependiendo las veces que haya ganado, el mensaje final se escribe dinamicamente en ambas pantallas, mensaje de derrota para el que tuvo menos victorias al final y mensaje de victoria para el que tuvo mas victorias al final
 function combate() {
     clearInterval(intervalo)
 
@@ -509,6 +506,7 @@ function combate() {
     revisarVictorias()
 }
 
+// despues de terminar el combate evalua quien tuvo mas victorias para mostrar el mensaje de vicotoria en la pantalla de ese jugador, y el mensaje de derrota para el otro jugador
 function revisarVictorias() {
     if (victoriasJugador == victoriasEnemigo) {
         mensajeFinal("Mmmm mismas victorias? supongo que fue un empate...") 
@@ -519,6 +517,7 @@ function revisarVictorias() {
     }
 }
 
+// crea y muestra el mensaje de cada uno de los ataques lanzados por ambos jugadores
 function crearMensaje(resultado) {
     let nuevoAtaqueDelJugador = document.createElement("p")
     let nuevoAtaqueDelEnemigo = document.createElement("p")
@@ -531,7 +530,7 @@ function crearMensaje(resultado) {
     ataquesDelEnemigo.appendChild(nuevoAtaqueDelEnemigo)
 }
 
-
+// crea y muestra el mensaje final en ambas pantallas dependiendo del resultado del combate, y muestra el boton de reiniciar
 function mensajeFinal(resultadoFinal) {
 
     let nuevoMensajeBatalla = document.createElement("p")
@@ -543,17 +542,18 @@ function mensajeFinal(resultadoFinal) {
     sectionReiniciar.style.display = "flex"
 }
 
+// reinicia el juego completo, debido a que reinicia la pagina
 function reiniciarJuego() {
-    combateEjecutado = false
     enMapa = false 
     location.reload()
 }
 
-
+// funcion que usa una operacion matematica para generar un numero aleatorio entre un rango definido, se asigna un rango distinto dependiendo de la necesidad
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+// pinta la mascota y el mapa, la mascota se pinta con la diferencia de que se pintan las coordenadas en tiempo real de la mascot y poder llamar a la funcion "enviarPosicion" con esta informacion
 function pintarMascotaYJuego() {
     miPersonaje.x = miPersonaje.x + miPersonaje.velocidadX
     miPersonaje.y = miPersonaje.y + miPersonaje.velocidadY
@@ -578,6 +578,7 @@ function pintarMascotaYJuego() {
     })
 }
 
+// envia la posicion en tiempo real de la mascota al servidor, para que otros jugadores puedan ver el movimiento de las mascotas. Tambien evalua si la mascota enemiga existe, si no existe crea todos los valores necesarios para que la mascota exista y todo funcione correctamente
 function enviarPosicion(x, y) { 
     fetch(`${API_URL}/elemental/${jugadorId}/posicion`, { 
         method: "post", 
@@ -594,27 +595,32 @@ function enviarPosicion(x, y) {
                 res.json() 
                 .then(function ({ enemigos }) { 
                     console.log(enemigos)
-                    personajesEnemigos = []
                     personajesEnemigos = enemigos.map(function (enemigo) { 
                         if (enemigo.mascota != undefined) { 
-                            let mascotaEnemiga = null 
+                            let mascotaEnemiga
                             const mascotaNombre = enemigo.mascota.nombre || ""
-                            if (mascotaNombre === "Aquanut") { 
-                                mascotaEnemiga = new Personaje("Aquanut", "./assets/aquanut-juego.png", 3, "./assets/aquanut-cara.png", enemigo.id) 
-                            } else if (mascotaNombre === "Drakon") { mascotaEnemiga = new Personaje("Drakon", "./assets/drakon-personaje.png", 3, "./assets/drakon-cara.png", enemigo.id) 
-
-                            } else if (mascotaNombre === "Selvatron") { mascotaEnemiga = new Personaje("Selvatron", "./assets/Selvatron-personaje.png", 3, "./assets/selvatron-cara.png", enemigo.id) 
-                            } 
-                            mascotaEnemiga.x = enemigo.x 
-                            mascotaEnemiga.y = enemigo.y 
-                            return mascotaEnemiga 
-                            } 
-                        }) 
+                            
+                            if (mascotaNombre === "Aquanut") {
+                                mascotaEnemiga = new Personaje("Aquanut", "./assets/aquanut-juego.png", 3, "./assets/aquanut-cara.png", enemigo.id)
+                            } else if (mascotaNombre === "Drakon") {
+                                mascotaEnemiga = new Personaje("Drakon", "./assets/drakon-personaje.png", 3, "./assets/drakon-cara.png", enemigo.id)
+                            } else if (mascotaNombre === "Selvatron") {
+                                mascotaEnemiga = new Personaje("Selvatron", "./assets/Selvatron-personaje.png", 3, "./assets/selvatron-cara.png", enemigo.id)
+                            }
+                            
+                            if (mascotaEnemiga) {
+                                mascotaEnemiga.x = enemigo.x 
+                                mascotaEnemiga.y = enemigo.y 
+                                return mascotaEnemiga 
+                            }
+                        } 
+                    }).filter(enemigo => enemigo != undefined) 
                     }) 
                 } 
             }) 
         }
 
+// funcion con condicional switch que evalua que tecla fisica fue presionada en un disposisitivo no movil para ejecutar cierta funcion para cada grupo de casos y mover al personaje segun la tecla presionada
 function teclas(event) {
     switch (event.key) {
         case "ArrowUp":
@@ -642,6 +648,7 @@ function teclas(event) {
     }
 }
 
+// funciones para el movimiento del personaje, tambien se reinicia la inactividad cuando alguna de las funciones se ejecuta
 function moverArriba() {
     miPersonaje.velocidadY = -5
     ultimoMovimiento = Date.now()
@@ -667,6 +674,7 @@ function detenerMovimiento() {
     miPersonaje.velocidadY = 0
 }
 
+// lo mismo de arriba pero para el movimiento en dispositivo movil
 btnArriba.addEventListener("touchstart", e => {
   e.preventDefault()
   moverArriba()
@@ -707,6 +715,7 @@ btnDerecha.addEventListener("touchend", e => {
   detenerMovimiento()
 })
 
+// obtiene la mascota del jugador
 function obtenerObjetoMascota() {
     for (let i = 0; i < personajes.length; i++) {
         if (guardarMascota === personajes[i].nombre) {
@@ -715,6 +724,7 @@ function obtenerObjetoMascota() {
     }
 }
 
+// funcion que detecta colision entre 2 jugadores
 function colisiones(enemigo) {
     const arribaMascota = miPersonaje.y
     const abajoMascota = miPersonaje.y + miPersonaje.alto
